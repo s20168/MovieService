6a6bc94746a830b356955d6d70f6e0da39b5066b
s20168 <s201682@pjwstk.edu.pl>
2021-05-09 15:25:49 +0200

    Added getMapping getMovieInfo

--- src/main/java/pl/pjatk/MovieService/Controller/MovieController.java
+++ src/main/java/pl/pjatk/MovieService/Controller/MovieController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.pjatk.MovieService.Model.Movie;
 
@@ -16,6 +17,11 @@ public class MovieController {
     @GetMapping("/Movies")
     public ResponseEntity<List<Movie>> getAll() {
         return new ResponseEntity<List<Movie>>(HttpStatus.OK);
+    }
+
+    @GetMapping("/Movies/{id}")
+    public Movie getMovieInfo(@PathVariable("movieId")String movieId) {
+        return new Movie(movieId, "Test name");
     }
 
 
